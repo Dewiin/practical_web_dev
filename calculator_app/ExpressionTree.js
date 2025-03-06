c9e0f846ec58509db0a1dbf3e38b5946fd4721be
Devin Xie <devinxie418@gmail.com>
2025-03-05 21:22:40 -0500

    implemented calculating logic, and added styling

---
+++ calculator_app/ExpressionTree.js
@@ -0,0 +1,53 @@
+class Node {
+    constructor(value, left, right) {
+        this.value = value;
+        this.left = left || null;
+        this.right = right || null;
+    }
+}
+
+export class ExpressionTree {
+    constructor(node) {
+        this.root = node || null;
+    }
+
+    
+    isOperator(char) {
+        return ['+', '-', '*', '/'].includes(char);
+    }
+
+    constructTree(postfixExpression) {
+        const stack = [];
+        const tokens = postfixExpression.split(" ");
+
+        for (const token of tokens) {
+          if (!this.isOperator(token)) {
+            stack.push(new Node(token));
+          } else {
+            const right = stack.pop();
+            const left = stack.pop();
+            const operatorNode = new Node(token, left, right);
+            stack.push(operatorNode);
+          }
+        }
+      
+        this.root = stack.pop();
+        return this.root;
+    }
+
+    evaluateTree(node = this.root) {
+        if (!node.left && !node.right) {
+            return parseFloat(node.value);
+        }
+    
+        const leftValue = this.evaluateTree(node.left);
+        const rightValue = this.evaluateTree(node.right);
+    
+        switch (node.value) {
+            case '+': return leftValue + rightValue;
+            case '-': return leftValue - rightValue;
+            case '*': return leftValue * rightValue;
+            case '/': return leftValue / rightValue;
+        }
+    }
+}
--- calculator_app/calculator.js
+++ calculator_app/calculator.js
@@ -1,20 +1,147 @@
-// const numberButtons = document.querySelectorAll(".number_button");
-// const data = {
-//   runningSum: "",
-// };
+import { ExpressionTree } from "./ExpressionTree.js"
 
-// function updateView(elementId) {
-//   document.getElementById(elementId).textContent = data.runningSum;
-//   console.log("updating view", data.runningSum);
-// }
+const numbersArr = Array.from(document.getElementsByClassName("number"));
+const operatorsArr = Array.from(document.getElementsByClassName("operator"));
+const parenthesisArr = Array.from(document.getElementsByClassName("parenthesis"));
+const numberStrs = "0123456789".split("");
 
-// function updateRunningSum(value) {
-//   data.runningSum += value;
-//   updateView("runningSum");
-// }
+const data = {
+    displayString: "0",
+    appendStringNum: (newChar) => {
+        if (newChar === ".") {
+        //decimal + 0?
+        }
 
-// numberButtons.forEach((button) => {
-//   button.addEventListener("click", (e) => {
-//     updateRunningSum(e.target.textContent);
-//   });
-// });
+        if (numberStrs.includes(newChar)) {
+            if (data.displayString === "0") {
+                data.displayString = "";
+            }
+            data.displayString += newChar;
+        }
+        data.updateDisplay();
+    },
+
+    appendStringOperator: (newChar) => {
+        if ((numberStrs.includes(data.displayString.slice(-1)) || data.displayString.slice(-1) === ")")) {
+            data.displayString += newChar;
+        }
+        data.updateDisplay();
+    },
+
+    appendStringParenthesis: (newChar) => {
+        if (newChar === ")") {
+        if (data.displayString.split("(").length <= data.displayString.split(")").length) {
+            //if there are not enough open parenthesis to add a closing parenthesis terminate early
+            return;
+        }
+        if (operatorsArr.map((operator) => operator.innerText).includes(data.displayString[data.displayString.length - 1])) {
+            //early termination for adding parenthesis after operator
+            return;
+        }
+        }
+        if (data.displayString === "0") {
+            data.displayString = "";
+        }
+
+        data.displayString += newChar;
+
+        data.updateDisplay();
+    },
+
+    updateDisplay: () => {
+        const display = document.getElementById("display");
+        display.innerText = data.displayString;
+    }, 
+
+    infixToPostfix: (expression) => {
+        const precedence = {
+            '*': 2,
+            '/': 2,
+            "+": 1,
+            "-": 1  
+        }
+
+        const stack = [];
+        let output = "";
+        let numBuffer = "";
+
+        for(let i = 0; i < expression.length; i++) {
+            let char = expression[i];
+
+            if(numberStrs.includes(char)) {
+                numBuffer += char;
+            }
+            else {
+                if(numBuffer) {
+                    output += numBuffer + " ";
+                    numBuffer = "";
+                }
+
+                // if character is an operator
+                if (char in precedence) {
+                    while (stack.length && (precedence[char] <= precedence[stack[stack.length - 1]])) {
+                        output += stack.pop() + " ";
+                    }
+                    stack.push(char);
+                }
+                else if (char === "(") {
+                    stack.push(char);
+                }
+                else if (char === ")") {
+                    while (stack.length && stack[stack.length - 1] !== '(') {
+                        output += stack.pop();
+                    } 
+                    stack.pop();
+                }
+            }
+        }
+
+        if(numBuffer) {
+            output += numBuffer + " ";
+        }
+
+        while(stack.length) {
+            output += stack.pop() + " ";
+        }
+        
+        return output.trim();
+    },
+
+    calculateDisplay: (infixExpression) => {
+        const postfixExpression = data.infixToPostfix(infixExpression);
+        const root = new ExpressionTree();
+
+        root.constructTree(postfixExpression);
+
+        data.displayString = "";
+        data.displayString += root.evaluateTree();
+        data.updateDisplay();
+    }
+};
+
+const display = document.getElementById("display");
+display.innerText = data.displayString;
+
+numbersArr.map((elem) => {
+    elem.addEventListener("click", () => {
+        data.appendStringNum(elem.innerText);
+    });
+});
+
+operatorsArr.map((elem) => {
+    elem.addEventListener("click", () => {
+        data.appendStringOperator(elem.innerText);
+    });
+});
+
+parenthesisArr.map((elem) => {
+    elem.addEventListener("click", () => {
+        data.appendStringParenthesis(elem.innerText);
+    });
+});
+
+const equalsOperator = document.querySelector(".equals");
+
+equalsOperator.addEventListener("click", () => {
+    data.calculateDisplay(data.displayString);
+})
